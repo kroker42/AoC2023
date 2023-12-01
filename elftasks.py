1e7a1a00cbc7555bb5465d35f586dc37ba3b7b7c
kroker42 <git@frisch.se>
2023-12-01 17:40:36 +0000

    day 1 - find digits in a string

--- elftasks.py
+++ elftasks.py
@@ -1,5 +1,6 @@
 import string
 import time
+import re
 from collections import namedtuple
 from collections import deque
 
@@ -17,14 +18,49 @@ from operator import mod
 import numpy
 from numpy import sign
 
+def match_digits(data):
+    matches = {str(i): str(i) for i in range(1, 10)}
+    matches["one"] = "1"
+    matches["two"] = "2"
+    matches["three"] = "3"
+    matches["four"] = "4"
+    matches["five"] = "5"
+    matches["six"] = "6"
+    matches["seven"] = "7"
+    matches["eight"] = "8"
+    matches["nine"] = "9"
+
+    nums = []
+
+    for l in data:
+        nums.append([])
+        i = 0
+        while i < len(l):
+            for digit in matches:
+                new_i = 0
+                x = re.findall("^" + digit, l[i:])
+                if x:
+                    nums[-1].append(matches[x[0]])
+                    new_i = len(x[0])
+                    break
+            if new_i <= 1:
+                i += 1
+            else:
+                i += new_i - 2
+    return nums
 
 
 def day1():
-    data = [line.strip().split() for line in open('input01.txt')]
+    data = [line.strip() for line in open('input01.txt')]
     start_time = time.time()
 
-    task1 = None
-    task2 = None
+    nums = [[x for x in i if x.isdigit()] for i in data]
+    nums = [int(x[0] + x[-1]) for x in nums]
+    task1 = sum(nums)
+
+    nums = [int(x[0] + x[-1]) for x in match_digits(data)]
+    task2 = sum(nums)
 
     return time.time() - start_time, task1, task2
     
+
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 import elftasks
+import re
 import numpy
 from io import StringIO
 from collections import deque
@@ -11,5 +12,16 @@ if __name__ == '__main__':
 
 
 class TestDay1(unittest.TestCase):
+    data = "\
+two1nine \
+eightwothree \
+abcone2threexyz \
+xtwone3four \
+4nineeightseven2 \
+zoneight234 \
+7pqrstsixteen"
     def test_task1(self):
-        self.assertEqual(False, elftasks.day1())
+        nums = elftasks.match_digits(self.data.split())
+        self.assertEqual(["219", "823", "123", "2134", "49872", "18234", "76"], ["".join(x) for x in nums])
+
+
